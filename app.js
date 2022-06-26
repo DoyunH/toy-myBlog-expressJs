a55c04c3df1407bd8ad2c7b717944301be52b8e4
Doyun <doyun@talktomeinkorean.com>
2022-06-26 20:27:01 +0900

    feature: make code clean with functions(templateHTML, templateListMaker)

--- app.js
+++ app.js
@@ -2,6 +2,37 @@ const http = require("http");
 const fs = require("fs");
 const url = require("url");
 
+const templateHTML = (title, filelist, body) => {
+  const list = templateListMaker(filelist);
+
+  return `
+    <!doctype html>
+      <html>
+      <head>
+        <title>WEB1 - ${title ? title : "Welcome"}</title>
+        <meta charset="utf-8">
+      </head>
+      <body>
+        <h1><a href="/">WEB</a></h1>
+        <ol>
+          ${list}
+        </ol>
+        ${body}
+      </body>
+      </html>
+    `;
+};
+
+const templateListMaker = (filelist) => {
+  const list = filelist
+    .map((file) => {
+      return `<li><a href='/?id=${file}'>${file}</a></li>`;
+    })
+    .join("\n");
+  console.log("filelist :", filelist);
+  return list;
+};
+
 const app = http.createServer(function (request, response) {
   let _url = request.url;
   let query = url.parse(_url, true).query;
@@ -12,30 +43,12 @@ const app = http.createServer(function (request, response) {
   if (pathname === "/") {
     fs.readdir("./data", function (err, filelist) {
       fs.readFile(`./data/${query.id}`, "utf8", function (err, description) {
-        const template = `
-      <!doctype html>
-        <html>
-        <head>
-          <title>WEB1 - ${title ? title : "Welcome"}</title>
-          <meta charset="utf-8">
-        </head>
-        <body>
-          <h1><a href="/">WEB</a></h1>
-          <ol>
-            ${
-              filelist &&
-              filelist.map(
-                function (file) {
-                  return `<li><a href='/?id=${file}'>${file}</a></li>`;
-                }.join("\n")
-              )
-            }
-          </ol>
-          <h2>${title ? title : "Welcome"}</h2>
-          <p>${description ? description : "Hello, Node.js"}</p>
-        </body>
-        </html>
-      `;
+        const template = templateHTML(
+          title,
+          filelist,
+          `<h2>${title ? title : "Welcome"}</h2>
+        <p>${description ? description : "Hello, Node.js"}</p>`
+        );
         response.writeHead(200);
         response.end(template);
       });
