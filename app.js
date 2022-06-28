b4921d2f6b9a09bf66180b1792ffdba774fb2a99
Doyun <doyun@talktomeinkorean.com>
2022-06-28 21:24:14 +0900

    feature: post redirection and update setting

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const url = require("url");
 const qs = require("querystring");
 
-const templateHTML = (title, filelist, body) => {
+const templateHTML = (title, filelist, body, control) => {
   const list = templateListMaker(filelist);
 
   return `
@@ -18,7 +18,7 @@ const templateHTML = (title, filelist, body) => {
         <ol>
           ${list}
         </ol>
-        <a href="/create">create</a>
+        ${control}
         ${body}
       </body>
       </html>
@@ -31,7 +31,6 @@ const templateListMaker = (filelist) => {
       return `<li><a href='/?id=${file}'>${file}</a></li>`;
     })
     .join("\n");
-  console.log("filelist :", filelist);
   return list;
 };
 
@@ -40,16 +39,22 @@ const app = http.createServer(function (request, response) {
   let query = url.parse(_url, true).query;
   // url info check
   let pathname = url.parse(_url, true).pathname;
+  let path = url.parse(_url, true).path;
   let title = query.id;
 
   if (pathname === "/") {
+    console.log(url.parse(_url, true));
     fs.readdir("./data", function (err, filelist) {
       fs.readFile(`./data/${query.id}`, "utf8", function (err, description) {
         const template = templateHTML(
           title,
           filelist,
-          `<h2>${title ? title : "Welcome"}</h2>
-        <p>${description ? description : "Hello, Node.js"}</p>`
+          `<h2>${title ? title : "Welcome"}</h2> <p>${
+            description ? description : "Hello, Node.js"
+          }</p>`,
+          path === "/"
+            ? '<a href="/create">create</a>'
+            : `<a href="/create">create</a><br><a href="/update?id=${title}">update</a>`
         );
         response.writeHead(200);
         response.end(template);
@@ -66,7 +71,8 @@ const app = http.createServer(function (request, response) {
         <p><input type='text' name='title' placeholder='title'></p>
         <p><textarea name='description' placeholder='description'></textarea></p>
         <p><input type='submit'></p>
-        </form>`
+        </form>`,
+        ""
       );
       response.writeHead(200);
       response.end(template);
@@ -78,11 +84,15 @@ const app = http.createServer(function (request, response) {
     });
     request.on("end", function () {
       const post = qs.parse(body);
-      console.log(post.title);
-      console.log(post.description);
+      const title = post.title;
+      const description = post.description;
+
+      // post file write
+      fs.writeFile(`./data/${title}`, description, "utf8", function (err) {
+        response.writeHead(302, {Location: `/?id=${title}`});
+        response.end();
+      });
     });
-    response.writeHead(200);
-    response.end("success");
   } else {
     response.writeHead(404);
     response.end("Not found");
